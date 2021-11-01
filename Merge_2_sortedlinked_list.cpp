b486829ceece3d705991d8c915ec0f9de68c3ea4
Rohit Yadav <83593699+RohitYadav0420@users.noreply.github.com>
2021-11-01 12:21:47 +0530

    merging 2 sorted linkedlist using iterative method

---
+++ Merge_2_sortedlinked_list.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newNode = new Node(data);
+		if (head == NULL)
+		{
+			head = newNode;
+			tail = newNode;
+		}
+		else
+		{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
+{
+    //Write your code here
+    if(head1==NULL && head2==NULL)
+    {
+        return head1;
+    }
+    else if(head1==NULL)
+    {
+        return head2;
+    }
+    else if(head2==NULL)
+    {
+        return head1;
+    }
+    
+    Node* headfinal=NULL;
+    Node* temp;
+    
+    if(head1->data <= head2->data)
+    {
+        headfinal= head1;
+        temp=head1;
+        head1=head1->next;
+    }
+    else 
+    {
+        headfinal=head2;
+        temp=head2;
+        head2=head2->next;
+    }
+    
+    while(head1!=NULL && head2!=NULL)
+    {
+        if(head1->data<=head2->data)
+        {
+            temp->next=head1;
+            temp=temp->next;
+            head1=head1->next;
+        }
+        else
+        {
+            temp->next=head2;
+            temp=temp->next;
+            head2=head2->next;
+        }
+    }
+    
+    if(head1==NULL)
+    {
+        temp->next=head2;
+    }
+    else 
+    {
+        temp->next=head1;
+    }
+    
+    return headfinal;
+    
+    
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head1 = takeinput();
+		Node *head2 = takeinput();
+		Node *head3 = mergeTwoSortedLinkedLists(head1, head2);
+		print(head3);
+	}
+	return 0;
+}
